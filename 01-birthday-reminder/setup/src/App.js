74355aa58ce740ef96992d3e3dc0d33dc9d02b31
Norman Munge <normanmunge@gmail.com>
2021-12-24 11:58:02 +0300

    Birthday reminder project:

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+
+  return (
+    <main>
+      <section className='container'>
+        <List people={people} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const List = () => {
+const List = ({ people }) => {
+  const [guys, setGuys] = useState(people);
+  const removeItem = (id) => {
+    console.log("THE ID:", id, "THE PEOPLE", guys);
+    let newPeople = guys.filter((person) => person.id !== id);
+    setGuys(newPeople);
+  };
   return (
     <>
-      <h2>list component</h2>
+      <h3>{guys.length} birthdays today</h3>
+      {guys.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <article key={id} className='person'>
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+            <button type='button' onClick={() => removeItem(id)}>
+              remove
+            </button>
+          </article>
+        );
+      })}
+      <button onClick={() => setGuys([])}>Clear All</button>
     </>
   );
 };
